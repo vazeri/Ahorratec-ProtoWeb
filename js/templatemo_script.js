7396707211480e4ac68ad9af67b681f0c22cf1c7
Goat <erick@ahorratec.org>
2017-08-06 02:51:07 -0500

    Update templatemo_script.js

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -1,28 +1,28 @@
 var top_menu_height = 0;
-jQuery(function($) {
-		$(window).load( function() {
-			$('.external-link').unbind("click");	
-		});
-		
-        $(document).ready( function() {
+jQuery(function ($) {
+    $(window).load(function () {
+        $('.external-link').unbind("click");
+    });
 
-            // load google map
+    $(document).ready(function () {
+
+        // load google map
         // scroll spy to auto active the nav item
         $('body').scrollspy({ target: "#templatemo-nav-bar", offset: top_menu_height + 10 });
-		$(".external-link").unbind("click");
+        $(".external-link").unbind("click");
 
         // scroll to top
-        $("#btn-back-to-top").click(function(e){
+        $("#btn-back-to-top").click(function (e) {
             e.preventDefault();
             scrollTo("#templatemo-top");
         });
 
         // scroll to specific id when click on menu
-        $('.templatemo-top-menu .navbar-nav a').click(function(e){
-            e.preventDefault(); 
+        $('.templatemo-top-menu .navbar-nav a').click(function (e) {
+            e.preventDefault();
             var linkId = $(this).attr('href');
             scrollTo(linkId);
-            if($(".navbar-toggle").is(":visible") === true){
+            if ($(".navbar-toggle").is(":visible") === true) {
                 $(".navbar-collapse").collapse('toggle');
             }
             $(this).blur();
@@ -33,26 +33,26 @@ jQuery(function($) {
         $(".templatemo-top-menu ").stickUp();
 
         // gallery category
-        $(".templatemo-gallery-category a").click(function(e){
-            e.preventDefault(); 
+        $(".templatemo-gallery-category a").click(function (e) {
+            e.preventDefault();
             $(this).parent().children("a").removeClass("active");
             $(this).addClass("active");
             var linkClass = $(this).attr("href");
-            $(".gallery").each(function(){
-                if($(this).is(":visible") === true){
-                   $(this).hide();
+            $(".gallery").each(function () {
+                if ($(this).is(":visible") === true) {
+                    $(this).hide();
                 };
             });
-            $(linkClass).fadeIn();  
+            $(linkClass).fadeIn();
         });
-      });
+    });
 
-// scroll animation 
-function scrollTo(selectors)
-{
+    // scroll animation 
+    function scrollTo(selectors) {
 
-    if(!$(selectors).size()) return;
-    var selector_top = $(selectors).offset().top - top_menu_height;
-    $("html,body").animate({ scrollTop: selector_top }, 'slow');
+        if (!$(selectors).size()) return;
+        var selector_top = $(selectors).offset().top - top_menu_height;
+        $("html,body").animate({ scrollTop: selector_top }, 'slow');
 
-}
+    }
+});
